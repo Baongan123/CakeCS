db4bc4b31dde70a72aad3dea3894768de481aded
baongan <buingan93@gmail.com>
2020-07-13 10:39:21 +0700

    .

--- StreetFood/Startup.cs
+++ StreetFood/Startup.cs
@@ -41,12 +41,12 @@ namespace StreetFood
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseExceptionHandler("/Error");
-                app.UseStatusCodePagesWithRedirects("/Error/{0}");
+                
             }
             else
             {
-                
+                app.UseExceptionHandler("/Error");
+                app.UseStatusCodePagesWithRedirects("/Error/{0}");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
