0353ea59ff6c2d53a8d7eef2f0a2f54aa0e64689
baongan <buingan93@gmail.com>
2020-07-14 14:55:35 +0700

    phapkung

--- StreetFood/Startup.cs
+++ StreetFood/Startup.cs
@@ -56,7 +56,7 @@ namespace StreetFood
             app.UseRouting();
             app.UseAuthentication();
             app.UseAuthorization();
-            
+            //phapkhung
 
             app.UseEndpoints(endpoints =>
             {
